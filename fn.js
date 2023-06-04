d522a708996c357060fa6f495d74d02aa1b7dc02
gipyeung <128315517+gipyeung@users.noreply.github.com>
2023-06-05 04:28:09 +0900

    A

---
+++ fn.js
@@ -0,0 +1,5 @@
+const fn ={
+    plus: (num1,num2) => num1 + num2,
+};
+
+module.exports = fn;
---
+++ fn.test.js
@@ -0,0 +1,5 @@
+const fn = require("./fn");
+
+test("더하기",()=>{
+    expect(fn.plus(1,2)).toBe(3);
+});
